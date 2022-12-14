1f8f06296cef1ddfff2945a1dd38c9d0346f3870
ranga-pacyfier <srirangarajanbalaji@gmail.com>
2022-12-14 22:29:49 +0530

    Initial commit

---
+++ department website/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(5000, (err)=>{
+    console.log("Connected");
+})
